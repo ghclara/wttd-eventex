c3afd36e0a9a16cb96a4ff0fabeecbe1f759c2ba
Ana Clara <clara.aclr@gmail.com>
2016-09-19 21:47:58 -0300

    email

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.template.loader import render_to_string
+
+from eventex import settings
 
 from .forms import SubscriptionForm
 
@@ -6,4 +12,25 @@ from .forms import SubscriptionForm
 def subscribe(request):
     context = {}
     context['form'] = SubscriptionForm()
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+
+        if not form.is_valid():
+            return render(
+                request,
+                'subscriptions/subscription_form.html',
+                {'form': form}
+            )
+
+        body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+
+        send_mail(
+            'Confirmação de Inscrição',
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [form.cleaned_data['email']],
+        )
+
+        messages.success(request, 'Inscrição realizada com sucesso!')
+        return HttpResponseRedirect('/inscricao/')
     return render(request, 'subscriptions/subscription_form.html', context)
