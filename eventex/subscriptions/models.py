c282a564c81957035142f92b71702cca1faa3d81
Ana Clara <clara.aclr@gmail.com>
2016-09-29 22:16:02 -0300

    admin

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 class Subscription(models.Model):
 
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11)
+    email = models.EmailField('Email')
+    phone = models.CharField('Telefone', max_length=20)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
     class Meta:
-        verbose_name = "Subscription"
-        verbose_name_plural = "Subsriptions"
+        verbose_name = "Inscrição"
+        verbose_name_plural = "Inscrições"
+        ordering = ('-created_at', )
 
     def __str__(self):
         return self.name
