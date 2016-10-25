3fb6eb8e7919ada45dbdb6910e842eac9028f810
Ana Clara <clara.aclr@gmail.com>
2016-10-25 21:50:26 -0200

    adiciona campo paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,9 +5,18 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'susbcribed_today')
+    list_display = (
+        'name',
+        'email',
+        'phone',
+        'cpf',
+        'created_at',
+        'susbcribed_today',
+        'paid',
+    )
     date_hierarchy = 'created_at'
     search_fields = ('name', 'cpf')
+    list_filter = ('paid', 'created_at')
 
     def susbcribed_today(self, obj):
         return obj.created_at == now().date()
