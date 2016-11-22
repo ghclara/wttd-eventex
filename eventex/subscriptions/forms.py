e3cdadc950b5425dd32f4ce67c33c832e14776e5
Ana Clara <clara.aclr@gmail.com>
2016-11-22 20:36:00 -0200

    email ou telefone opcionais

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -14,9 +14,15 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome:')
     cpf = forms.CharField(label='CPF:', validators=[validate_cpf])
-    email = forms.CharField(label='Email:')
-    phone = forms.CharField(label='Telefone:')
+    email = forms.CharField(label='Email:', required=False)
+    phone = forms.CharField(label='Telefone:', required=False)
 
     def clean_name(self):
-        name = self.clean_name.get('name')
+        name = self.cleaned_data.get('name')
         return name.title()
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou Telefone')
+
+        return self.cleaned_data
